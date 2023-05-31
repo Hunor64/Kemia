e2684f34a912521b01d366b90632a3dc3302b865
Nhunor64 <nhunor06@gmail.com>
2023-05-31 09:32:23 +0200

    Félidő

---
+++ Dolgozat/Felfedezesek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolgozat
+{
+        class Felfedezesek
+    {
+        string ev;
+        string elem;
+        string vegyjel;
+        int rendszam;
+        string felfedezo;
+        
+        public Felfedezesek(string ev,string elem,string vegyjel,int rendszam,string felfedezo)
+        {
+            this.ev = ev;
+            this.elem = elem;
+            this.vegyjel = vegyjel;
+            this.rendszam = rendszam;
+            this.felfedezo = felfedezo;
+        }
+        public Felfedezesek(string sor)
+        {
+            var sorFelbontva = sor.Split(';');
+            this.ev = sorFelbontva[0];
+            this.elem = sorFelbontva[1];
+            this.vegyjel = sorFelbontva[2];
+            this.rendszam = int.Parse(sorFelbontva[3]);
+            this.felfedezo = sorFelbontva[4];
+        }
+        public string Ev { get => ev; }
+        public string Element { get => elem; }
+        public string Vegyjel { get => vegyjel; }
+        public int Rendszam { get => rendszam; }
+        public string Felfedezo { get => felfedezo; }
+        
+    }
+}
--- Dolgozat/Program.cs
+++ Dolgozat/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using System.IO;
 
 namespace Dolgozat
 {
@@ -11,6 +12,58 @@ namespace Dolgozat
     {
         static void Main(string[] args)
         {
+            //2
+            List<Felfedezesek> felfedezeseks = new List<Felfedezesek>();
+            string fileLocation = "felfedezesek.csv";
+            var sorok = File.ReadAllLines(fileLocation);
+            int index = 0;
+            foreach (var elem in sorok)
+            {
+                if (index != 0)
+                {
+                    Felfedezesek uj = new Felfedezesek(elem);
+                    felfedezeseks.Add(uj);
+                }
+                index++;
+            }
+            //3
+            Console.WriteLine($"3. Feladat: Elemek száma: {felfedezeseks.Count}");
+
+            //4
+            int okorElemek = 0;
+            foreach (var elem in felfedezeseks)
+            {
+                if (elem.Ev == "Ókor")
+                {
+                    okorElemek++;
+                }
+            }
+            Console.WriteLine($"4. Feladat: Felfedezések száma az ókorban: {okorElemek}");
+
+            //5
+            string eredmeny;
+            while (true)
+            {
+                Console.Write("5. Feladat: Kérek egy vegyjelet: ");
+                eredmeny = Console.ReadLine();
+                if (eredmeny.Length == 1 || eredmeny.Length == 2)
+                {
+                    break;
+                }
+            }
+
+            //6
+            foreach (var elem in felfedezeseks)
+            {
+                if (elem.Vegyjel.ToLower() == eredmeny.ToLower())
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("6. Feldat: Nincs ilyen elem az adatforrásban!");
+                }
+            }
         }
     }
 }
